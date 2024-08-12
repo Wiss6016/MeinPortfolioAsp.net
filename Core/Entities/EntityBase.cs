211a595fff50dd208eb02da63525c06f87cfe125
Wiss6016 <immerlyw@gmail.com>
2024-08-12 21:30:01 +0200

    Projektdateien hinzufügen.

---
+++ Core/Entities/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Core.Entities
+{
+    internal class EntityBase
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entities
+{
+    internal class Owner : EntityBase
+    {
+        public string? FullName  { get; set; }
+        public string? Profil { get; set; }
+        public int Avatar { get; set; }
+        public Address? Address { get; set; }
+    }
+}
